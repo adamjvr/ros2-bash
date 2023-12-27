23c1004b48482c36401ad944ba5b1ad97270a95d
Adam Vadala-Roth <fenderstratman@gmail.com>
2023-12-27 17:31:10 -0500

    Specific and Verbose
    Updated the script to be more specific and verbose and less error prone. Also updated to run on freshly installed system to further automate installation and make easier for new users.

--- src/install_ros2.sh
+++ src/install_ros2.sh
@@ -16,22 +16,47 @@
 # You should have received a copy of the GNU General Public License along with
 # this program.  If not, see <http://www.gnu.org/licenses/>.
 
+#!/bin/bash
+
+# Update Ubuntu
+sudo apt update && sudo apt upgrade -y
+
 # Set the ROS2 distribution
 ROS_DISTRO=humble
 
-# Set the locale
+# Check for UTF-8 locale
+locale  # check for UTF-8
+
+# Install locales package and set locale
+sudo apt install -y locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+# Verify locale settings
+locale  # verify settings
+
+# Ensure Ubuntu Universe repository is enabled
+sudo apt install -y software-properties-common
+sudo add-apt-repository universe
+
+# Update and install curl
+sudo apt update && sudo apt install -y curl
+
+# Add the ROS 2 repository key
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Add the ROS 2 repository
-sudo sh -c "echo 'deb [arch=amd64,arm64] http://packages.ros.org/ros2/ubuntu focal main' > /etc/apt/sources.list.d/ros2.list"
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+echo "deb [arch=amd64,arm64 signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Update the package list
 sudo apt update
 
-# Install ROS 2 packages
+# Install ROS 2 Desktop packages
 sudo apt install -y ros-$ROS_DISTRO-desktop
+
+# Install ROS 2 Development tools
+sudo apt install -y ros-$ROS_DISTRO-desktop-development
 
 # Source the ROS 2 setup file
 source /opt/ros/$ROS_DISTRO/setup.bash
@@ -45,6 +70,8 @@ pip3 install -U argcomplete
 # Source the ROS 2 setup file in the bashrc
 echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
 
-# Install additional ROS 2 packages or tools as needed
+# Environment setup
+echo "source /opt/ros/$ROS_DISTRO/setup.bash" >> ~/.bashrc
+source ~/.bashrc
 
 echo "ROS 2 installation complete!"
